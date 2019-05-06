00dffb7bddf31e94098980c9dc1d42aadf5deb8b
annaH7777777 <49167607+annaH7777777@users.noreply.github.com>
2019-05-06 23:14:42 +0300

    Starting and ending functionality added

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,9 @@
 public class MainClass {
     public static void main(String[] args) {
+        System.out.println("application started");
         int a  = 85;
         int b =  a/10 + (a%10);
         System.out.println("summ is " + b);
+        System.out.println("application ended");
     }
 }
